a33a9d7fe2b0b42ce3a436320d2711ab87c8d17b
Krasimir Hristozov <krasi.hristozov@gmail.com>
2018-11-19 13:56:00 +0000

    Added CORS support to API

--- trivia-web-service/app/Http/Kernel.php
+++ trivia-web-service/app/Http/Kernel.php
@@ -35,11 +35,13 @@ class Kernel extends HttpKernel
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
+            \Barryvdh\Cors\HandleCors::class,
         ],
 
         'api' => [
             'throttle:60,1',
             'bindings',
+            \Barryvdh\Cors\HandleCors::class,
         ],
     ];
 
