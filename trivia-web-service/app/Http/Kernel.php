e87ee9ddbfa59bceb73f3bb223ed3810dfe333de
Krasimir Hristozov <krasi.hristozov@gmail.com>
2018-11-19 14:57:54 +0000

    Added Okta authentication to the backend API

--- trivia-web-service/app/Http/Kernel.php
+++ trivia-web-service/app/Http/Kernel.php
@@ -42,6 +42,7 @@ class Kernel extends HttpKernel
             'throttle:60,1',
             'bindings',
             \Barryvdh\Cors\HandleCors::class,
+            \App\Http\Middleware\AuthenticateWithOkta::class,
         ],
     ];
 
